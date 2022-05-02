adb4151f09a14b42d7443ac7aadf4f929ea5eb5d
shinusmp <shrinivaspatil12@gmail.com>
2022-05-02 22:53:41 +0530

    changes after stash

--- com/bikkad/demo/Rahul.java
+++ com/bikkad/demo/Rahul.java
@@ -6,8 +6,13 @@ public class Rahul {
 	private void syso() {
 		// TODO Auto-generated method stub
 
+<<<<<<< Updated upstream
 ///heloo
 //heelooo
+=======
+//hiiiiii
+//hiiii
+>>>>>>> Stashed changes
 
 	}
 
